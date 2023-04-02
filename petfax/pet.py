a11be3fbd71b1bf21733e387c0f75f0d8f93cdff
Glindley23 <graham.lindley23@gmail.com>
2023-04-02 13:55:49 -0400

    added new facts form

--- petfax/pet.py
+++ petfax/pet.py
@@ -2,7 +2,7 @@ from flask import ( Blueprint, render_template )
 import json
 
 pets = json.load(open('pets.json'))
-print(pets)
+
 
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 
@@ -13,4 +13,4 @@ def index():
 @bp.route('/<int:id>')
 def show(id):
     pet = pets[id - 1]
-    return render_template('pets/show.html', pet=pet)
+    return render_template('show.html', pet=pet)
